f38805ac2fdb38570b25cb5ce2c6e1ec36c9f3b0
CaptainRinoXD <dinhdainhan1@gmail.com>
2024-03-27 23:06:37 +0700

    sucubus boss

---
+++ Assets/Scripts/CombatCore/Meele.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class Meele : MonoBehaviour
+{
+    [SerializeField] float knockBackAmout;
+    
+    [Header("Player")]
+    [SerializeField] GameObject PlayerObject;
+    [SerializeField] float damgeGivenToEnemy;
+
+    [Header("Enemy")]
+    [SerializeField] GameObject EnemyObject;
+    [SerializeField] float damgeGivenToPlayer;
+    [SerializeField] public float attackCoolDown;
+
+    private BoxCollider2D MyBoxCollider2D;
+    private float direction;
+
+    private void Awake()
+    {
+        MyBoxCollider2D = GetComponent<BoxCollider2D>();
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            direction = PlayerObject.transform.localScale.x; //Get the direction where the player hit
+            collision.GetComponent<Health>().TakeDamage(damgeGivenToEnemy); //Damge enemy health
+
+            //Slime
+            if (collision.GetComponent<SlimeMV>() != null)
+            {
+                collision.GetComponent<SlimeMV>().enabled = false;
+                collision.GetComponent<SlimeMV>().isBeingHit(true, direction, knockBackAmout);
+            }
+
+            //Sucubus
+            if (collision.GetComponent<Sucu_mv>() != null)
+            {
+                collision.GetComponent<Sucu_mv>().enabled = false;
+                collision.GetComponent<Sucu_mv>().isBeingHit(true, direction, knockBackAmout);
+            }
+            
+            // lý do vì sao lấy được GetCompoent<Health> vì được lấy từ đối tượng va vào colider của gameObject này.
+            MyBoxCollider2D.enabled = false;
+        }
+        if (collision.tag == "Player")
+        {
+            print("True");
+            StartCoroutine(meeleAttackCoolDown(collision));
+
+        }
+        else if (collision.tag == "DetectionZone" || collision.tag == "InvisibleWall") //ingore out the collsion for dectionZone
+        {
+            //print("Collsion dectect"); //https://forum.unity.com/threads/ignore-collisions-by-tag-solved.60387/
+            if (collision.GetComponent<CircleCollider2D>() != null)
+                Physics2D.IgnoreCollision(collision.GetComponent<CircleCollider2D>(), GetComponent<BoxCollider2D>(), true);
+        }
+    }
+
+    private IEnumerator meeleAttackCoolDown(Collider2D collision)
+    {
+        direction = EnemyObject.transform.localScale.x;
+        EnemyObject.GetComponent<Sucu_Attack>().AttackNear(collision, damgeGivenToPlayer, direction, knockBackAmout);
+        yield return new WaitForSeconds(0.2f);
+        MyBoxCollider2D.enabled = false;
+        yield return new WaitForSeconds(attackCoolDown);
+        MyBoxCollider2D.enabled = true;
+    }
+}
---
+++ Assets/Scripts/CombatCore/Sucu_Attack.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sucu_Attack : MonoBehaviour
+{
+    [SerializeField] public float attackFarCoolDown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireBalls;
+
+    [SerializeField] GameObject attackNearBoxCollider2D;
+    private Animator MyAnimator;
+    private PlayerMovement playerMovement;
+
+    private float attkNearCoolDown;
+    private void Start()
+    {
+        //attackNearBoxCollider2D.GetComponent<BoxCollider2D>().enabled = false; //set GameObject for MeeleColider to off
+    }
+    private void Awake()
+    {
+        MyAnimator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+    public void Update()
+    {
+        attkNearCoolDown += Time.deltaTime;
+        //coolDownTimer += Time.deltaTime;
+        //coolDownTimer = coolDownTimer + Time.deltaTime;
+
+        //print(attkNearCoolDown);
+        /*Lấy AttackNearCoolDown làm timer. Khi phương thức AttackNear() được gọi thì attackNearCoolDown = 0
+         * Cho đến khi nào attakNearCoolDown lớn hơn 1 thì mới thực hiện việc tắt. Nhược điểm của cách làm này so với
+         * IEnumerator là bặt buộc  phải cho điêu kiện vào trong FixUpdate Gây đến việc giảm hiệu quả. Còn nếu
+         * Sử dụng IEnumerator trong ngay phương thức AttackNear sẽ hiệu quả hơn.
+         */
+        //if (attkNearCoolDown > 0.2f)
+            //attackNearBoxCollider2D.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void AttackNear(Collider2D collision,float damgeGivenToPlayer, float _direction, float knockBackAmout)
+    {
+
+        MyAnimator.SetTrigger("attackNear");
+        collision.GetComponent<Health>().TakeDamage(damgeGivenToPlayer);
+        collision.GetComponent<PlayerMovement>().isBeingHit(true, _direction, knockBackAmout * 1.5f);
+        attkNearCoolDown = 0;
+    }
+
+    public void Attack()
+    {
+        MyAnimator.SetTrigger("attackFar");
+        fireBalls[findBall()].transform.position = firePoint.position;
+        fireBalls[findBall()].GetComponent<Projectile>().setDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int findBall()
+    {
+        for (int i = 0; i < fireBalls.Length; i++)
+        {
+            if (!fireBalls[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+}
