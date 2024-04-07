38cea2ebdecf2fd58a92b713f7d737631b2db229
verylowpower <doudqwer@gmail.com>
2024-04-08 01:55:30 +0700

    check_point

---
+++ Assets/Scripts/CheckPoint/CheckPoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{   
+    private Respawn respawn;
+
+    private void Awake()
+    {
+        respawn = GameObject.FindGameObjectWithTag("Respawn").GetComponent<Respawn>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            respawn.Bonfire = this.gameObject;
+        }
+    }
+
+}
